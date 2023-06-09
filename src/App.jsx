1c29e13ed73d6f03b53a5a19462e50272ec96f5c
Andrew Lohman <andrewjlohman@gmail.com>
2023-06-09 13:02:44 -0600

    data manipulation app

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import Select from "react-select"
+import EmployeeCard from './assets/components/EmployeeCard'
+import axios from "axios"
+import './App.css'
+
+function App() {
+
+  const [text, setText] = useState("")
+  const [employees, setEmployees] = useState([])
+
+  const mappedRes = (res) => {
+    
+  }
+
+  useEffect(() => {
+    axios.get('https://hub.dummyapis.com/employee?noofRecords=100&idStarts=1001')
+    .then(res=> 
+      // console.log(res.data)
+     
+      setEmployees(res.data)
+      
+      ).catch(err=> console.log(err))
+  }, []);
+
+
+  
+  const order = [
+    {value: 'id', label: 'ID'},
+    {value: 'name', label: 'Name'},
+    {value: 'dob', label: 'DOB'},
+    {value: 'salary', label: 'Salary'}
+  ]
+
+  return (
+    
+      <div className='page'>
+        <div className='table'>
+          <div className='tableTop'>
+            <div className='select-container'>
+              <Select options={order} placeholder="Order By"/>
+            </div>
+            <input type='text' onChange={()=>setText} value={text} placeholder="Search Database"/>
+          </div>
+        <div className='tableCategory'> 
+          <div className='empName'>
+            <h3>Last Name:</h3>
+          </div>
+          <div className='empName'>
+            <h3>First Name:</h3>
+          </div>
+          <div className='empId'>
+            <h3>Id:</h3>
+          </div>
+          <div className='empAge'>
+            <h3>Age:</h3>
+          </div>
+          <div className='empDob'>
+            <h3>DOB:</h3>
+          </div>
+          <div className='empEmail'>
+            <h3>Email:</h3>
+          </div>
+          <div className='empNumber'>
+            <h3>Phone Number:</h3>
+          </div>
+          <div className='empSalary'>
+            <h3>Salary:</h3>
+          </div>
+          <div className='empAddress'>
+            <h3>Address</h3>
+          </div>
+            </div>
+          <div className='tableContent'>
+            {employees?.map((employee)=> {
+              return(
+                <EmployeeCard key={employee.id} employee={employee}/>
+                    )
+            })}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/EmployeeCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const EmployeeCard = ({employee}) => {
+ console.log(employee)
+    return(
+    <div className='employee-card'>
+        <div className='empName'>
+            <h3>{employee.lastName}</h3>
+        </div>
+        <div className='empName'>
+            <h3>{employee.firstName}</h3>
+        </div>
+        <div className='empId'>
+            <h3>{employee.id}</h3>
+        </div>
+        <div className='empAge'>
+            <h3>{employee.age}</h3>
+        </div>
+        <div className='empDob'>
+            <h3>{employee.dob}</h3>
+        </div>
+        <div className='empEmail'>
+            <h3>{employee.email}</h3>
+        </div>
+        <div className='empNumber'>
+            <h3>{employee.contactNumber}</h3>
+        </div>
+        <div className='empSalary'>
+            <h3>{employee.salary}</h3>
+        </div>
+        <div className='empAddress'>
+            <h3>{employee.address}</h3>
+        </div>
+    </div>
+ )
+}
+
+export default EmployeeCard
